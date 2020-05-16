dab79704ae1cca5b5b392999f85b5ee4c3793090
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2020-05-16 01:57:58 -0300

    Updates

---
+++ precache-manifest.343bf46934a379565dcd576a563bffd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b407f8e4f5b387cdc3c4eb69ba1f6b08",
+    "url": "/covidFollowUp/index.html"
+  },
+  {
+    "revision": "5061c50e0d177c89883d",
+    "url": "/covidFollowUp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3759ee3f257118714375",
+    "url": "/covidFollowUp/static/js/2.a32c4707.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covidFollowUp/static/js/2.a32c4707.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5061c50e0d177c89883d",
+    "url": "/covidFollowUp/static/js/main.45acaef6.chunk.js"
+  },
+  {
+    "revision": "0519094be110c8c6c351",
+    "url": "/covidFollowUp/static/js/runtime-main.e4cafcb0.js"
+  }
+]);
